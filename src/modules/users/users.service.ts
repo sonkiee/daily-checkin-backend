9e3233e795a5a715a5df1a062a58b99a21ce3d74
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 03:20:31 +0000

    Jul 27, 2025, 4:20 AM

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,34 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../config/db";
+import { NewUser, Users } from "./users.schema";
+
+class UserService {
+  async findAll(): Promise<Users[]> {
+    return db.select().from(Users);
+  }
+
+  async findById(id: string): Promise<Users | null> {
+    const user = await db.select().from(Users).where(eq(Users.id, id));
+    return user.length > 0 ? user[0] : null;
+  }
+
+  async create(user: NewUser): Promise<Users> {
+    const [newUser] = await db.insert(Users).values(user).returning();
+    return newUser;
+  }
+
+  async update(id: string, userData: Partial<NewUser>): Promise<Users | null> {
+    const [updatedUser] = await db
+      .update(Users)
+      .set(userData)
+      .where(eq(Users.id, id))
+      .returning();
+    return updatedUser || null;
+  }
+
+  async delete(id: string): Promise<void> {
+    await db.delete(Users).where(eq(Users.id, id));
+  }
+}
+
+export const userService = new UserService();
