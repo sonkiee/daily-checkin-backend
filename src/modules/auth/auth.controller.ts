44407a5cdfd2327a6923ea70f756b74b9c9762db
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 04:29:07 +0000

    Jul 27, 2025, 5:29 AM

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express";
+import jwt from "../../utils/jwt";
 
-const refreshToken = (req: Request, res: Response) => {};
+const refreshToken = (req: Request, res: Response) => {
+  const token = req.body.refreshToken;
+
+  if (!token)
+    return res.status(400).json({
+      error: "No refresh token provided",
+    });
+
+  const { valid, expired, decoded } = jwt.verify(token);
+
+  if (!valid) {
+    return res.status(401).json({
+      error: expired ? "Refresh token expired" : "Invalid refresh token",
+    });
+  }
+
+  const newToken = jwt.generate({ id: decoded!.id });
+  res.status(200).json(newToken);
+};
 
 export { refreshToken };
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,21 +1,23 @@
-import jwt, { JwtPayload, TokenExpiredError } from "jsonwebtoken";
+import jwt, { JwtPayload as Payload, TokenExpiredError } from "jsonwebtoken";
 import "dotenv/config";
+
+interface JwtPayload extends Payload {
+  id: string;
+  deviceId?: string;
+}
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
   throw new Error("Missing JWT_SECRET in environment variables");
 }
 
-const sign = (payload: object): string => {
-  return jwt.sign(payload, JWT_SECRET as string, {
-    expiresIn: "7d",
-  });
-};
-
-const refresh = (payload: object): string => {
-  return jwt.sign(payload, JWT_SECRET as string, {
-    expiresIn: "15min",
-  });
+const generate = (payload: JwtPayload) => {
+  const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" });
+  const refreshToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
 
 const verify = (
@@ -23,14 +25,22 @@ const verify = (
 ): {
   valid: boolean;
   expired: boolean;
-  decoded?: JwtPayload | string;
+  decoded?: JwtPayload;
 } => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET as string);
+
+    if (typeof decoded === "object" && decoded !== null && "id" in decoded) {
+      return {
+        valid: true,
+        expired: false,
+        decoded: decoded as JwtPayload,
+      };
+    }
+
     return {
-      valid: true,
+      valid: false,
       expired: false,
-      decoded: decoded,
     };
   } catch (error) {
     return {
@@ -40,4 +50,4 @@ const verify = (
   }
 };
 
-export default { sign, verify, refresh };
+export default { generate, verify };
