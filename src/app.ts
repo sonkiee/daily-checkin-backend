bdf112a641aaaba5f4321860045f58d6acf842f9
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 06:57:57 +0000

    Jul 27, 2025, 7:57 AM

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import compression from "compression";
 import router from "./routes/index.routes";
 
 const app = express();
-
 app.use(express.json());
+
 app.use(cookieParser());
 app.use(compression());
 
