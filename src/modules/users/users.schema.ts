b812511012c3664822ece52d796dd38c84ea4670
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 02:46:56 +0000

    Jul 27, 2025, 3:46 AM

--- src/modules/users/users.schema.ts
+++ src/modules/users/users.schema.ts
@@ -4,8 +4,18 @@ import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core"; // ✅ 
 const Users = pgTable("users", {
   id: uuid("id").primaryKey(),
   deviceId: varchar("device_id", { length: 64 }).notNull(),
+  totalPoints: varchar("total_points", { length: 10 }).notNull().default("0"),
+  currentStreak: varchar("current_streak", { length: 10 })
+    .notNull()
+    .default("0"),
+  longestStreak: varchar("longest_streak", { length: 10 })
+    .notNull()
+    .default("0"),
+  lastCheckin: timestamp("last_checkin").notNull(),
+  isActive: varchar("is_active", { length: 10 }).notNull().default("true"),
   pushToken: varchar("push_token", { length: 256 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
 
 export type Users = InferSelectModel<typeof Users>;
