48547ca2a654bc24b0719fd7024b8b0ed9461398
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 02:43:38 +0000

    Jul 27, 2025, 3:43 AM

--- src/modules/users/users.schema.ts
+++ src/modules/users/users.schema.ts
@@ -4,6 +4,7 @@ import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core"; // ✅ 
 const Users = pgTable("users", {
   id: uuid("id").primaryKey(),
   deviceId: varchar("device_id", { length: 64 }).notNull(),
+  pushToken: varchar("push_token", { length: 256 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
